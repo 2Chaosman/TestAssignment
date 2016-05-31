4486e3c888bbd963fa2997d70b559451673b13d2
Sergey M <2chaosman@gmail.com>
2016-05-31 21:04:37 +0300

    Change angularController.js

--- src/js/partials/angularController.js
+++ src/js/partials/angularController.js
@@ -17,6 +17,20 @@ angular.module('testAssignment', [])
 
         console.log($scope.student);
 
+        var msg = $('#ResultsForm').serialize();
+        $.ajax({
+            type: 'POST',
+            url: 'http://localhost:9000/res.php',
+            data: msg,
+            success: function (data) {
+                $('#serverResult').html(data);
+            },
+            error: function (xhr, str) {
+                alert('Возникла ошибка: ' + xhr.responseCode);
+            }
+        });
+
+
     }
 
 
